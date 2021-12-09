c74a56dc1ba787f54b9445606b7e213e61f508da
mcnallyj1037 <jmcnally@mcnallysoftware.com>
2021-12-08 22:40:24 -0500

    Initial commit of reminder project

---
+++ stickynotes/src/test/java/com/edwardjones/stickynotes/StickynotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edwardjones.stickynotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StickynotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
